9e62e498ee4917c583fe8a4488e03f2058bccced
celisun <sunceli@brandeis.edu>
2017-10-12 01:12:27 -0400

    Update _left_or_right_hand_rule.py

--- policy/_left_or_right_hand_rule.py
+++ policy/_left_or_right_hand_rule.py
@@ -1,4 +1,5 @@
 ## Policy known by scan twist center control node
+## can be either left or right hand rule. 
 
 import math
 
@@ -10,7 +11,7 @@ class LeftOrRightHandRule:
         self.type = type
         self.direction = 1 if type=='LHR' else -1
         self.turn_speed = turn_speed
-        self.decision = None
+        self.decision = ''
 
 
     def step(self, space_ahead, angle_with_closet_obstacle):
@@ -19,18 +20,22 @@ class LeftOrRightHandRule:
 
         if (space_ahead < 2):
             angular_z = self.direction * self.turn_speed            # Make turns. distance at front front less than 2 desired distance_to_wall
-
+            self.decision = 'Turn'
+            
         elif (space_ahead < 4):
             linear_x_factor = 0.5                                          # Ahead. distance at front less than 4 desired distance_to_wall
-
+            self.decision = 'Ahead'
+            
         elif (math.fabs(angle_with_closet_obstacle) > 1.75):        # Slow ahead
             linear_x_factor = 0.4
-
+            self.decision = 'Slow ahead'
         else:
             linear_x_factor = 1                                # Full speed ahead
+            self.decision = 'Full speed ahead'
 
-
+        self.pirnt_status()
         return linear_x_factor, angular_z
-
-
-
+    
+    def print_status():
+        print ("Decision: ")
+        print (self.decision)
